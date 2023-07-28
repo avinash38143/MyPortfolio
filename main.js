4262117147fec24c64ab5e860761614532c8fde0
Avinash Pandey <114892174+avinash38143@users.noreply.github.com>
2023-07-28 10:00:40 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed('#element', {
+    strings: ['Web Developer', 'Competitive Programmer', 'Software Developer'],
+    typeSpeed: 100,
+    backSpeed: 70,
+    backDelay: 1000,
+    loop: true
+  });
